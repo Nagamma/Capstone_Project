b77871658593b0d4c00a6c5a1491bf41701cef84
Nagamma <nagamma06@gmail.com>
2026-05-13 11:37:11 +0530

    logging file added to the project

--- core_scripts/data_extraction.py
+++ core_scripts/data_extraction.py
@@ -1,35 +1,48 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-#database conncetions
+import logging
 
+#Logging to both console and file
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler('logs/etljob.log'),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+#database conncetions
 #Target DB connection
 mysql_engine = create_engine('mysql+pymysql://root:admin%401990@localhost:3306/Feb2026Retaildwh')
 
 class DataExtraction:
     def extract_product_data_load_stag(self):
-        print("Product data extraction started.....")
+        logger.info("Product data extraction started.....")
         df = pd.read_csv("source_systems/product_data.csv")
         df.to_sql("stag_product",mysql_engine, index=False)
-        print("Product data extraction finished and loaded into staging area....")
+        logger.info("Product data extraction finished and loaded into staging area....")
 
     def extract_sales_data_load_stag(self):
-        print("Sales data extraction started.....")
+        logger.info("Sales data extraction started.....")
         df = pd.read_csv("source_systems/sales_data_linux.csv")
         df.to_sql("stag_sales",mysql_engine, index=False)
-        print("Sales data extraction finished and loaded into staging area....")
+        logger.info("Sales data extraction finished and loaded into staging area....")
 
     def extract_inventory_data_load_stag(self):
-        print("Inventory data extraction started.....")
+        logger.info("Inventory data extraction started.....")
         df=pd.read_xml("source_systems/inventory_data.xml",xpath=".//item",parser="etree")
         df.to_sql("stag_inventory",mysql_engine, index=False)
-        print("Inventory data extraction finished and loaded into staging area....")
+        logger.info("Inventory data extraction finished and loaded into staging area....")
 
     def extract_suplier_data_load_stag(self):
-        print("Supplier data extraction started.....")
+        logger.info("Supplier data extraction started.....")
         df = pd.read_json("source_systems/supplier_data.json")
         df.to_sql("stag_suplier",mysql_engine, index=False)
-        print("Supplier data extraction finished and loaded into staging area....")
+        logger.info("Supplier data extraction finished and loaded into staging area....")
 
 
 if __name__ == "__main__":
